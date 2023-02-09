dcc41abda22ce641d8db8d5f8bbd54431d3da668
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-09 13:24:35 +0300

    Create getter and setter methods for dog and person classes.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Dog
+    
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    # attr_accessor :name, :breed
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
 
 end
+
+# binding.pry
+
+d1 = Dog.new
+d1.name="Twingo"
+d1.breed="Mastiff"
+
+puts d1
+
